07621386a5b63f0455a34ae077f898532d8cbac2
nicolascadima <nicolas.cadima@gmail.com>
2015-07-17 19:07:07 -0400

    esto es una prueba

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim lWSEDV As New WSEDV.Servicios
+        lWSEDV.Registrar(TextBox1.Text)
+        TextBox1.Text = 1
+        'Prueba
+    End Sub
+End Class
